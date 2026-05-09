5bee39a55b4ac7f7abb236d4eac5ba2e9ba48d0b
zimbabwe1 <tyresefreeman99@gmail.com>
2026-05-08 21:29:28 -0700

    New project

---
+++ javascript/calandertest/calander.js
@@ -0,0 +1,4 @@
+const now = new Date()
+console.log("Current date and time: " + now)
+const futureDate = new Date(); futureDate.setDate(futureDate.getDate() + 7)
+console.log("Date and time one week from now: " + futureDate)
